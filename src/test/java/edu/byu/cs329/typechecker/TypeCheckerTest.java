1f9fd0ecf31b4a2c93fda7a048eb06129fc70ede
Eric Mercer <egm@cs.byu.edu>
2019-10-22 11:40:59 -0600

    Remove unused imports

--- src/test/java/edu/byu/cs329/typechecker/TypeCheckerTest.java
+++ src/test/java/edu/byu/cs329/typechecker/TypeCheckerTest.java
@@ -1,23 +1,9 @@
 package edu.byu.cs329.typechecker;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.stream.Stream;
 
-import org.eclipse.jdt.core.JavaCore;
-import org.eclipse.jdt.core.dom.AST;
-import org.eclipse.jdt.core.dom.ASTNode;
-import org.eclipse.jdt.core.dom.ASTParser;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DynamicNode;
 import org.junit.jupiter.api.DynamicTest;
