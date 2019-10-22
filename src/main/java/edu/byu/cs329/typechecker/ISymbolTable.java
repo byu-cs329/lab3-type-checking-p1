1f05502c89f0824eb12cb6420d188280ed3946bb
Eric Mercer <egm@cs.byu.edu>
2019-10-22 11:41:28 -0600

    Simplify symbol table interface

--- src/main/java/edu/byu/cs329/typechecker/ISymbolTable.java
+++ src/main/java/edu/byu/cs329/typechecker/ISymbolTable.java
@@ -13,22 +13,22 @@ public interface ISymbolTable {
   public static final String STRING_OBJECT = "String";
   public static final String OBJECT = "Object";
 
-  public String getFieldType(Deque<String> className, String fieldName);
+  public String getFieldType(String className, String fieldName);
 
-  public String getMethodReturnType(Deque<String> className, String methodName);
+  public String getMethodReturnType(String className, String methodName);
 
-  public String getParameterType(Deque<String> className, String methodName,
+  public String getParameterType(String className, String methodName,
       String paramName);
 
   public String getLocalType(String name); 
 
-  public boolean classExists(Deque<String> className);
+  public boolean classExists(String className);
 
-  public boolean fieldExists(Deque<String> className, String fieldName);
+  public boolean fieldExists(String className, String fieldName);
 
-  public boolean methodExists(Deque<String> className, String methodName);
+  public boolean methodExists(String className, String methodName);
 
-  public boolean parameterExists(Deque<String> className, String methodName,
+  public boolean parameterExists(String className, String methodName,
       String paramName);
   
   public boolean localExists(String name);
