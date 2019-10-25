da51e8a8507cb416c35a89f264bc7a37ceee74c3
Eric Mercer <egm@cs.byu.edu>
2019-10-25 15:20:39 -0600

    Add final writeup

--- src/test/java/edu/byu/cs329/typechecker/TypeCheckerTest.java
+++ src/test/java/edu/byu/cs329/typechecker/TypeCheckerTest.java
@@ -1,40 +1,38 @@
 package edu.byu.cs329.typechecker;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Iterator;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.List;
+import java.util.Objects;
 
-import org.junit.jupiter.api.Assertions;
+import org.eclipse.jdt.core.dom.ASTNode;
 import org.junit.jupiter.api.DynamicNode;
-import org.junit.jupiter.api.DynamicTest;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestFactory;
-import org.opentest4j.AssertionFailedError;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TypeCheckerTest {
   static final Logger log = LoggerFactory.getLogger(TypeCheckerTest.class);
-  
-  @TestFactory
-  Collection<DynamicNode> dynamicTestsFromCollection() {
-    return Arrays.asList(
-        DynamicTest.dynamicTest("1st dynamic test", () -> Assertions.assertTrue(true)),
-        DynamicTest.dynamicTest("2nd dynamic test", () -> Assertions.assertEquals(4, 2))
-    );
+
+  private static URI getURI(final String fileName) {
+    URL url = ClassLoader.getSystemResource(fileName);
+    Objects.requireNonNull(url, "\'" + fileName + "\'" + " not found in classpath");
+    URI uri = null;
+    try {
+      uri = url.toURI();
+    } catch (URISyntaxException e) {
+      log.error("Failed to get URI for" + fileName);
+      e.printStackTrace();
+    }
+    return uri;
   }
-  
-  @Test
-  void myTest() {
-    Collection<DynamicNode> tests = dynamicTestsFromCollection();
-    Iterator<DynamicNode> iter = tests.iterator();
-    DynamicNode n = iter.next();
-    Assertions.assertTrue(n instanceof DynamicTest);
-    DynamicTest t = (DynamicTest) n;
-    Assertions.assertDoesNotThrow(t.getExecutable());
-    n = iter.next();
-    Assertions.assertTrue(n instanceof DynamicTest);
-    t = (DynamicTest) n;
-    Assertions.assertThrows(AssertionFailedError.class, t.getExecutable());
+
+  private List<DynamicNode> generateProof(final String fileName, ISymbolTable symbolTable) {
+    URI fileURI = getURI(fileName);
+    ASTNode cu = Parser.parse(fileURI);
+
+    // TODO: instanciate visitor, call cu.accept(visitor), and return proof
+    return null;
   }
+
 }
